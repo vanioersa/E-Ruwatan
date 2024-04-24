50c28f4aace4a0cb2aca7056f3cb646e40a151ab
vanioersa <vanvanramadhan@gmail.com>
2024-04-24 07:34:39 +0700

    memperbaiki page yang akan di gunakan

---
+++ src/page/guru/guru.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardGuru() {
+  return (
+    <div>DashboardGuru</div>
+  )
+}
+
+export default DashboardGuru
---
+++ src/page/murid/murid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardMurid() {
+  return (
+    <div>DashboardMurid</div>
+  )
+}
+
+export default DashboardMurid
