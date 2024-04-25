e56ef06ee29cd335c64d62c4e29c7223cc5d911c
AndiSaputro50 <AndiSaputro50>
2024-04-25 14:57:56 +0700

    update style dashboard

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook, faChalkboardTeacher, faDoorOpen, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faChalkboardTeacher, faDoorOpen, faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Dashboard() {
   return (
@@ -13,30 +13,30 @@ function Dashboard() {
             {/* Kartu pertama */}
             <div className="mb-4 p-4 md:w-1/4 sm:w-1/2 w-full">
               <div className="ring-1 shadow-lg bg-sky-600 px-6 py-6 mx-5 rounded-lg flex items-center justify-between md:mt-16 md:my-12">
-                <FontAwesomeIcon icon={faBook} className="inline-block w-12 h-12 text-white mr-4" />
+                <FontAwesomeIcon icon={faUser} className="inline-block w-12 h-12 text-white mr-4" />
                 <div>
                   <h2 className="title-font font-medium text-3xl text-white">20</h2>
-                  <p className="leading-relaxed font-medium text-white">KBM Guru</p>
+                  <p className="leading-relaxed font-medium text-white">Siswa</p>
                 </div>
               </div>
             </div>
             {/* Kartu kedua */}
             <div className="mb-4 p-4 md:w-1/4 sm:w-1/2 w-full">
               <div className="ring-1 shadow-lg bg-blue-600 px-6 py-6 mx-5 rounded-lg flex items-center justify-between md:mt-16 md:my-12">
-                <FontAwesomeIcon icon={faUser} className="inline-block w-12 h-12 text-white mr-4" />
+                <FontAwesomeIcon icon={faDoorOpen} className="inline-block w-12 h-12 text-white mr-4" />
                 <div>
                   <h2 className="title-font font-medium text-3xl text-white">200</h2>
-                  <p className="leading-relaxed font-medium text-white">Piketan</p>
+                  <p className="leading-relaxed font-medium text-white">Kelas</p>
                 </div>
               </div>
             </div>
             {/* Kartu ketiga */}
             <div className="mb-4 p-4 md:w-1/4 sm:w-1/2 w-full">
               <div className="ring-1 shadow-lg bg-blue-600 px-6 py-6 mx-5 rounded-lg flex items-center justify-between md:mt-16 md:my-12">
-                <FontAwesomeIcon icon={faUser} className="inline-block w-12 h-12 text-white mr-4" />
+                <FontAwesomeIcon icon={faChalkboardTeacher} className="inline-block w-12 h-12 text-white mr-4" />
                 <div>
                   <h2 className="title-font font-medium text-3xl text-white">200</h2>
-                  <p className="leading-relaxed font-medium text-white">Piketan</p>
+                  <p className="leading-relaxed font-medium text-white">Guru</p>
                 </div>
               </div>
             </div>
