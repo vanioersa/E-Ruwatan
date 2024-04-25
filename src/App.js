7aa79370916ec49b7f6360e0f3bf3efa4d3060f2
AndiSaputro50 <AndiSaputro50>
2024-04-25 13:55:51 +0700

    menambah sidebar dan dashboard di role guru

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './auth/login';
 import RegisterSiswa from './auth/register_siswa';
 import RegisterGuru from './auth/register_guru';
 import DashboardSiswa from './component/Dashboard';
+import DashboardGuru from './component/DashboardGuru';
 import Siswa from './page/murid/siswa/Siswa';
 import Kelas from './page/murid/kelas/Kelas';
 import Guru from './page/murid/guru/Guru';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/register_siswa' element={<RegisterSiswa />} />
         <Route path='/register_guru' element={<RegisterGuru />} />
         <Route path='/dashboard_siswa/*' element={<DashboardSiswa />} exact />
+        <Route path='/dashboard_guru/*' element={<DashboardGuru />} exact />
         <Route path='/siswa' element={<Siswa />} exact />
         <Route path='/kelas' element={<Kelas />} exact />
         <Route path='/guru' element={<Guru />} exact />
---
+++ src/component/DashboardGuru.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import SidebarGuru from './SidebarGuru';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faUser } from '@fortawesome/free-solid-svg-icons';
+
+function Dashboard() {
+    return (
+        <div className="bg-gray-50 min-h-screen">
+            <SidebarGuru />
+            <section className="text-gray-800 body-font">
+                <div className="container mx-auto py-24">
+                    <div className="flex flex-wrap justify-center">
+                        {/* Kartu pertama */}
+                        <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
+                            <div className="ring-1 shadow-lg bg-sky-600 px-6 py-6 rounded-lg flex items-center justify-center">
+                                <FontAwesomeIcon icon={faBook} className="inline-block w-12 h-12 text-white mr-4" />
+                                <div>
+                                    <h2 className="title-font font-medium text-3xl text-white">20</h2>
+                                    <p className="leading-relaxed font-medium text-white">KBM Guru</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Kartu kedua */}
+                        <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
+                            <div className="ring-1 shadow-lg bg-blue-600 px-6 py-6 rounded-lg flex items-center justify-center">
+                                <FontAwesomeIcon icon={faUser} className="inline-block w-12 h-12 text-white mr-4" />
+                                <div>
+                                    <h2 className="title-font font-medium text-3xl text-white">200</h2>
+                                    <p className="leading-relaxed font-medium text-white">Piketan</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Tambahkan lebih banyak kartu jika diperlukan */}
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Dashboard;
