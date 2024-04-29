9c8bb0aa746b7ec5deff7a41b652e245c5fb66bc
vanvan <vanvanramadhan@gmail.com>
2024-04-29 18:57:23 +0700

    tambah siswa

--- src/page/murid/siswa/TambahSiswa.jsx
+++ src/page/murid/siswa/TambahSiswa.jsx
@@ -1,39 +1,39 @@
-import React, { useState } from "react";
-import Sidebar from "../../../component/Sidebar";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
-import { createSiswa } from "../";
+import axios from "axios";
+import Sidebar from "../../../component/Sidebar";
+import { createSiswa } from "./api_siswa";
 
 const TambahSiswa = () => {
-  const [murid, setMurid] = useState({
-    nama: "",
-    nik: "",
+  const [siswa, setSiswa] = useState({
+    nama_siswa: "",
     nisn: "",
-    lahir: "",
+    kelasId: "",
+    tempat: "",
     alamat: "",
   });
-  const [kelasJurusan, setKelasJurusan] = useState([]);
-  const [selectedKelasJurusan, setSelectedKelasJurusan] = useState("");
-  const history = useHistory();
+  const [kelas, setKelas] = useState([]);
+  const [selectedKelas, setSelectedKelas] = useState("");
+  const navigate = useNavigate();
 
-  const fetchKelasJurusan = async () => {
+  const fetchKelas = async () => {
     try {
-      const response = await axios.get(
-        "http://localhost:8080/tugas_akhir/api/kelas/all"
-      );
-      setKelasJurusan(response.data);
+      const response = await axios.get("http://localhost:4001/kelas/all");
+      setKelas(response.data);
     } catch (error) {
-      console.error("Failed to fetch Kelas and Jurusan: ", error);
+      console.error("Gagal mengambil data Kelas: ", error);
     }
   };
 
   useEffect(() => {
-    fetchKelasJurusan();
+    fetchKelas();
   }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setMurid((prevMurid) => ({
-      ...prevMurid,
+    setSiswa((prevSiswa) => ({
+      ...prevSiswa,
       [name]: value,
     }));
   };
@@ -52,7 +52,7 @@ const TambahSiswa = () => {
     }).then(async (result) => {
       if (result.isConfirmed) {
         try {
-          await createMurid({ ...murid, kelasId: selectedKelasJurusan });
+          await createSiswa({ ...siswa, kelasId: selectedKelas });
           Swal.fire({
             title: "Berhasil",
             text: "Murid berhasil ditambahkan",
@@ -60,17 +60,17 @@ const TambahSiswa = () => {
             showConfirmButton: false,
             timer: 2000,
           }).then(() => {
-            history.goBack();
+            navigate(-1);
           });
-          setMurid({
-            nama: "",
-            nik: "",
+          setSiswa({
+            nama_siswa: "",
             nisn: "",
-            lahir: "",
+            kelasId: "",
+            tempat: "",
             alamat: "",
           });
         } catch (error) {
-          console.error("Failed to add Murid: ", error);
+          console.error("Gagal menambahkan siswa: ", error);
           let errorMessage = "Gagal menambahkan murid. Silakan coba lagi.";
           if (
             error.response &&
@@ -91,6 +91,10 @@ const TambahSiswa = () => {
     });
   };
 
+  const batal = () => {
+    window.location.href = "/siswa"
+  }
+
   return (
     <div className="flex flex-col md:flex-row h-screen">
       <div className="sidebar w-full md:w-64">
@@ -106,16 +110,16 @@ const TambahSiswa = () => {
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 mt-2">
               <div className="relative">
                 <label
-                  htmlFor="nama"
+                  htmlFor="nama_siswa"
                   className="block mb-2 text-sm sm:text-xs font-medium text-gray-900"
                 >
                   Nama
                 </label>
                 <input
                   type="text"
-                  id="nama"
-                  name="nama"
-                  value={siswa.nama}
+                  id="nama_siswa"
+                  name="nama_siswa"
+                  value={siswa.nama_siswa}
                   onChange={handleChange}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Masukkan Nama Siswa"
@@ -146,37 +150,39 @@ const TambahSiswa = () => {
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 mt-2">
               <div className="relative">
                 <label
-                  htmlFor="kelas"
+                  htmlFor="kelasId"
                   className="block mb-2 text-sm sm:text-xs font-medium text-gray-900"
                 >
                   Kelas
                 </label>
                 <select
-                  id="kelas"
-                  name="kelas"
-                  value={siswa.kelas}
-                  onChange={handleChange}
+                  id="kelasId"
+                  name="kelasId"
+                  value={selectedKelas}
+                  onChange={(e) => setSelectedKelas(e.target.value)}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   required
                 >
-                  <option value="">Pilih Kelas</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
+                  <option value="">Pilih Kelas dan Nama Kelas</option>
+                    {kelas.map((kelas) => (
+                      <option key={kelas.id} value={kelas.id}>
+                        {`${kelas.kelas} - ${kelas.nama_kelas}`}
+                      </option>
+                    ))}
                 </select>
               </div>
               <div className="relative">
                 <label
-                  htmlFor="tempatLahir"
+                  htmlFor="tempat"
                   className="block mb-2 text-sm sm:text-xs font-medium text-gray-900"
                 >
                   Tempat Lahir
                 </label>
                 <input
                   type="text"
-                  id="tempatLahir"
-                  name="tempatLahir"
-                  value={siswa.tempatLahir}
+                  id="tempat"
+                  name="tempat"
+                  value={siswa.tempat}
                   onChange={handleChange}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="Masukkan Tempat Lahir"
