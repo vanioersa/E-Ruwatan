10e42aa91f1b4e512cb4b1b8ca3a808e42f46033
AndiSaputro50 <AndiSaputro50>
2024-05-01 13:59:57 +0700

    update style role murid pada tabel

--- src/page/murid/guru/TambahGuru.jsx
+++ src/page/murid/guru/TambahGuru.jsx
@@ -102,7 +102,7 @@ const TambahGuru = () => {
       </div>
       <div className="content-page max-h-screen container p-8 min-h-screen">
         <h1 className="judul text-3xl font-semibold">Tambah Guru</h1>
-        <div className="add-guru mt-12 bg-white p-5 ml-8 border border-gray-200 rounded-xl shadow-lg">
+        <div className="add-guru mt-12 md:mt-11 bg-white p-5 mr-0 md:ml-8 border border-gray-200 rounded-xl shadow-lg">
           <p className="text-lg sm:text-xl font-medium mb-4 sm:mb-7">
             Tambah Guru
           </p>
