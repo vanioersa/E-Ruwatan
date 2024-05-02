9928bc8aa8e25cd40821dcbe242eaffcbecfde09
vanvan <vanvanramadhan@gmail.com>
2024-05-02 08:59:46 +0700

    update icon sidebar role murid dan guru

--- src/component/SidebarGuru.jsx
+++ src/component/SidebarGuru.jsx
@@ -5,6 +5,8 @@ import {
   faRightToBracket,
   faUserGroup,
   faChalkboardTeacher,
+  faBars,
+  faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 import logobinus from "../asset/logobinus.png";
 import Swal from "sweetalert2";
@@ -76,10 +78,14 @@ function SidebarGuru() {
         <div className="flex items-center">
           <button
             id="sidebar-toggle"
-            className="text-black focus:outline-none md:hidden"
+            className="text-black focus:outline-none md:hidden mx-3"
             onClick={toggleSidebar}
           >
-            {/* Icon for sidebar toggle */}
+            {sidebarOpen ? (
+              <FontAwesomeIcon icon={faTimes} size="lg" />
+            ) : (
+              <FontAwesomeIcon icon={faBars} size="lg" />
+            )}
           </button>
           <img src={logobinus} className="h-12" alt="Logo" />
           <a href="/dashboard_guru">
@@ -91,8 +97,9 @@ function SidebarGuru() {
       </nav>
 
       <div
-        className={`fixed top-0 left-0 z-40 w-64 h-full bg-white shadow-xl border-r transition-transform duration-300 transform ${sidebarOpen ? "translate-x-0" : "-translate-x-full"
-          }`}
+        className={`fixed top-0 left-0 z-40 w-64 h-full bg-white shadow-xl border-r transition-transform duration-300 transform ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
       >
         <div className="bg-blue-800 text-white px-4 py-3">
           <h1 className="text-2xl font-semibold">E-RUWATAN</h1>
@@ -106,10 +113,11 @@ function SidebarGuru() {
             <li
               key={index}
               className={`py-2 px-3 my-2 rounded cursor-pointer 
-                      ${isActive(item.path)
-                  ? "bg-gray-400 text-black"
-                  : "hover:bg-gray-400 hover:text-black"
-                }`}
+                      ${
+                        isActive(item.path)
+                          ? "bg-gray-400 text-black"
+                          : "hover:bg-gray-400 hover:text-black"
+                      }`}
             >
               <button
                 onClick={() => handleNavigation(item.path)}
@@ -138,7 +146,11 @@ function SidebarGuru() {
           </li>
         </ul>
       </div>
-      <div className={`ml-0 md:ml-56 transition-transform duration-300 ease-in-out ${sidebarOpen ? "md:ml-0" : "-md:ml-64"}`} />
+      <div
+        className={`ml-0 md:ml-56 transition-transform duration-300 ease-in-out ${
+          sidebarOpen ? "md:ml-0" : "-md:ml-64"
+        }`}
+      />
     </div>
   );
 }
