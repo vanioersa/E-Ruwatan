dcaacc8babbb0fc3000bf0eabc7c5f801aed675f
AndiSaputro50 <AndiSaputro50>
2024-05-21 09:23:19 +0700

    update icon sidebar dan dashboard

--- src/component/SidebarGuru.jsx
+++ src/component/SidebarGuru.jsx
@@ -1,14 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHome,
-  faRightToBracket,
-  faUserGroup,
-  faChalkboardTeacher,
-  faBarsStaggered,
-  faXmark,
-  faStar,
-} from "@fortawesome/free-solid-svg-icons";
 import logobinus from "../asset/logobinus.png";
 import profil from "../asset/profil.png";
 import Swal from "sweetalert2";
@@ -30,19 +20,6 @@ function SidebarGuru() {
     };
 
     handleResize(); // Check initial window size
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 768) {
-        setSidebarOpen(false);
-      } else {
-        setSidebarOpen(true);
-      }
-    };
-
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
@@ -102,9 +79,14 @@ function SidebarGuru() {
             onClick={toggleSidebar}
           >
             {sidebarOpen ? (
-              <FontAwesomeIcon icon={faXmark} size="lg" />
+              <svg className="w-6 h-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
+              </svg>
             ) : (
-              <FontAwesomeIcon icon={faBarsStaggered} size="lg" />
+              <svg className="w-6 h-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+              </svg>
+
             )}
           </button>
           <img src={logobinus} className="h-12" alt="Logo" />
@@ -128,7 +110,7 @@ function SidebarGuru() {
             >
               <span className="absolute -inset-1.5"></span>
               <span className="sr-only">Open user menu</span>
-              <img className="h-8 w-8 rounded-full" src="#" alt="" />
+              <img className="h-8 w-8 rounded-full" src={profil} alt="" />
             </button>
           </div>
 
@@ -171,41 +153,84 @@ function SidebarGuru() {
           <h1 className="text-2xl font-semibold">E-RUWATAN</h1>
         </div>
         <ul className="mt-6 text-xl mx-2 text-gray-600">
-          {[
-            { name: "Dashboard", icon: faHome, path: "/dashboard_guru" },
-            { name: "KBM Guru", icon: faChalkboardTeacher, path: "/kbm_guru" },
-            { name: "Piketan", icon: faUserGroup, path: "/piketan_guru" },
-            { name: "Penilaian", icon: faStar, path: "/penilaian" },
-          ].map((item, index) => (
-            <li
-              key={index}
-              className={`py-2 px-3 my-2 rounded cursor-pointer 
-                      ${isActive(item.path)
-                  ? "bg-gray-400 text-black"
-                  : "hover:bg-gray-400 hover:text-black"
-                }`}
+          <li
+            className={`py-2 px-3 my-2 rounded cursor-pointer ${isActive("/dashboard_guru")
+              ? "bg-gray-400 text-black"
+              : "hover:bg-gray-400 hover:text-black"
+              }`}
+          >
+            <button
+              onClick={() => handleNavigation("/dashboard_guru")}
+              className="flex items-center w-full"
             >
-              <button
-                onClick={() => handleNavigation(item.path)}
-                className="flex items-center w-full"
-              >
-                <FontAwesomeIcon icon={item.icon} className="mr-2" />
-                <span
-                  style={{ fontFamily: "Segoe UI" }}
-                  className="mx-2 font-medium"
-                >
-                  {item.name}
-                </span>
-              </button>
-            </li>
-          ))}
+              <svg className="w-7 h-7 mr-2" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+              </svg>
+              <span style={{ fontFamily: "Roboto", fontWeight: "bold" }} className="mx-2">
+                Dashboard
+              </span>
+            </button>
+          </li>
+          <li
+            className={`py-2 px-3 my-2 rounded cursor-pointer ${isActive("/kbm_guru")
+              ? "bg-gray-400 text-black"
+              : "hover:bg-gray-400 hover:text-black"
+              }`}
+          >
+            <button
+              onClick={() => handleNavigation("/kbm_guru")}
+              className="flex items-center w-full"
+            >
+              <svg className="w-7 h-7 mr-2" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"></path>
+              </svg>
+              <span style={{ fontFamily: "Poopins", fontWeight: "bold" }} className="mx-2 font-medium">
+                KBM Guru
+              </span>
+            </button>
+          </li>
+          <li
+            className={`py-2 px-3 my-2 rounded cursor-pointer ${isActive("/piketan_guru")
+              ? "bg-gray-400 text-black"
+              : "hover:bg-gray-400 hover:text-black"
+              }`}
+          >
+            <button
+              onClick={() => handleNavigation("/piketan_guru")}
+              className="flex items-center w-full"
+            >
+              <svg className="w-7 h-7 mr-2" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"></path>
+              </svg>
+              <span style={{ fontFamily: "Poopins", fontWeight: "bold" }} className="mx-2 font-medium">
+                Piketan
+              </span>
+            </button>
+          </li>
+          <li
+            className={`py-2 px-3 my-2 rounded cursor-pointer ${isActive("/penilaian")
+              ? "bg-gray-400 text-black"
+              : "hover:bg-gray-400 hover:text-black"
+              }`}
+          >
+            <button
+              onClick={() => handleNavigation("/penilaian")}
+              className="flex items-center w-full"
+            >
+              <svg className="w-7 h-7 mr-2" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"></path>
+              </svg>
+              <span style={{ fontFamily: "Poopins", fontWeight: "bold" }} className="mx-2 font-medium">
+                Penilaian
+              </span>
+            </button>
+          </li>
           <li className="py-2 px-3 my-2 hover:text-black hover:bg-gray-400 rounded cursor-pointer absolute bottom-0 left-0 w-full">
             <button onClick={logout} className="flex items-center w-full">
-              <FontAwesomeIcon icon={faRightToBracket} className="mr-2 ml-3" />
-              <span
-                style={{ fontFamily: "Segoe UI" }}
-                className="mx-2 font-medium"
-              >
+              <svg className="w-7 h-7 mr-2 ml-3" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"></path>
+              </svg>
+              <span style={{ fontFamily: "Poopins", fontWeight: "bold" }} className="mx-2 font-medium">
                 Keluar
               </span>
             </button>
