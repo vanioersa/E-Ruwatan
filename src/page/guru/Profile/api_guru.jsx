16213f8eada336efe72bf9830a819098bd33909e
AndiSaputro50 <AndiSaputro50>
2024-05-22 15:29:44 +0700

    update edit profil guru

--- src/page/guru/Profile/api_guru.jsx
+++ src/page/guru/Profile/api_guru.jsx
@@ -2,31 +2,31 @@ import axios from "axios";
 
 const apiUrl = "http://localhost:4001";
 
-export const getAllAdmin = async () => {
+export const getAllGuru = async () => {
   try {
     const response = await axios.get(`${apiUrl}/users`);
     return response.data;
   } catch (error) {
-    throw new Error("Failed to fetch users");
+    throw new Error("Gagal mengambil data guru");
   }
 };
 
-export const getAdminById = async (id) => {
-    try {
-      const token = localStorage.getItem("token");
-      const response = await axios.get(`${apiUrl}/users/by-id/${id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error(`Failed to fetch user with id ${id}: `, error);
-      throw error;
-    }
-  };
+export const getGuruById = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(`${apiUrl}/users/by-id/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Gagal mengambil data guru dengan id ${id}: `, error);
+    throw error;
+  }
+};
   
- export const updateAdmin = async (id, userData) => {
+export const updateGuru = async (id, userData) => {
   try {
     const token = localStorage.getItem("token");
     const response = await axios.put(`${apiUrl}/users/update/${id}`, userData, {
@@ -36,7 +36,7 @@ export const getAdminById = async (id) => {
     });
     return response.data;
   } catch (error) {
-    console.error(`Failed to update user with id ${id}: `, error);
+    console.error(`Gagal memperbarui data guru dengan id ${id}: `, error);
     throw error;
   }
 };
