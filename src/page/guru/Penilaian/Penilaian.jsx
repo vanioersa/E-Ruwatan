82d6fb306661f3f198577f6923f81a7591bdf669
yuniarahmah <yuraama75@gmail.com>
2024-05-28 15:24:15 +0700

    update be_rahma

--- src/page/guru/Penilaian/Penilaian.jsx
+++ src/page/guru/Penilaian/Penilaian.jsx
@@ -9,6 +9,7 @@ import {
   faTrash,
   faEdit,
   faFileExport,
+  faUpload,
 } from "@fortawesome/free-solid-svg-icons";
 import ReactPaginate from "react-paginate";
 import axios from "axios";
@@ -23,13 +24,61 @@ function Penilaian() {
   const [currentPage, setCurrentPage] = useState(0);
   const [dataPerPage] = useState(10);
   const [kelas, setKelas] = useState([]);
-  const [Siswa, setSiswa] = useState([]);
-
+  const [siswa, setSiswa] = useState([]);
+  const [showImportModal, setShowImportModal] = useState(false);
   const pageCount = Math.ceil(data.length / dataPerPage);
+
+  // Tambahkan state baru untuk menangani file Excel yang diunggah
+  const [excelFile, setExcelFile] = useState(null);
+
+  // Buat fungsi untuk menangani perubahan pada file Excel yang diunggah
+  const handleExcelChange = (e) => {
+    setExcelFile(e.target.files[0]);
+  };
 
   const changePage = ({ selected }) => {
     setCurrentPage(selected);
   };
+
+  const openImportModal = () => {
+    setShowImportModal(true);
+  };
+  const closeImportModal = () => {
+    setShowImportModal(false);
+  };
+
+  const importExcell = async (e) => {
+    e.preventDefault();
+    if (!excelFile) {
+      Swal.fire("Error", "Anda belum memilih file untuk diimport!.", "error");
+      return;
+    }
+  
+    const formData = new FormData();
+    formData.append("file", excelFile);
+  
+    // Assuming you have a token stored in local storage or some other secure place
+    const token = localStorage.getItem("token");
+  
+    try {
+      const response = await axios.post(
+        "http://localhost:4001/panilaian/import",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`, // Add the token here
+          },
+        }
+      );
+      console.log(response.data);
+      Swal.fire("Sukses!", "Berhasil Ditambahkan.", "success");
+    } catch (error) {
+      console.error("Error importing file:", error);
+      // Log the error object to inspect its properties
+      Swal.fire("Error", "Gagal mengimpor file. " + error.message, "error");
+    }
+  };  
 
   useEffect(() => {
     fetchData();
@@ -104,7 +153,7 @@ function Penilaian() {
   };
 
   const filteredData = data.filter((item) => {
-    const namaSiswa = Siswa.find((s) => s.id === item.siswaId)?.nama_siswa;
+    const namaSiswa = siswa.find((s) => s.id === item.siswaId)?.nama_siswa;
     const kelass = kelas.find((k) => k.id === item.kelasId)?.kelas;
     const namaKelas = kelas.find((k) => k.id === item.kelasId)?.nama_kelas;
     return (
@@ -119,7 +168,7 @@ function Penilaian() {
 
   const dataToExport = filteredData.map((item, index) => ({
     No: index + 1,
-    "Nama Siswa": Siswa.find((s) => s.id === item.siswaId)?.nama_siswa,
+    "Nama Siswa": siswa.find((s) => s.id === item.siswaId)?.nama_siswa,
     Kelas: `${kelas.find((k) => k.id === item.kelasId)?.kelas} - ${
       kelas.find((k) => k.id === item.kelasId)?.nama_kelas
     }`,
@@ -205,7 +254,10 @@ function Penilaian() {
         <SidebarGuru />
       </div>
       <div className="content-page flex-1 container p-8 overflow-y-auto">
-        <div style={{ backgroundColor: "white" }} className="my-10 bg-white border border-gray-200 md:mt-20 mt-20 rounded-xl shadow-lg p-6">
+        <div
+          style={{ backgroundColor: "white" }}
+          className="my-10 bg-white border border-gray-200 md:mt-20 mt-20 rounded-xl shadow-lg p-6"
+        >
           <h1 className="text-3xl font-semibold text-gray-800">
             Penilaian Guru
           </h1>
@@ -225,13 +277,48 @@ function Penilaian() {
               </Link>
               <button
                 onClick={exportToXlsx}
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                className="bg-green-500 hover:bg-green-700 text-white px-2 py-2 mx-2 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
               >
-                <FontAwesomeIcon icon={faFileExport} /> Export
+                <FontAwesomeIcon icon={faFileExport} /> Export Piket
               </button>
+              <button
+                onClick={openImportModal}
+                className="bg-yellow-500 hover:bg-yellow-700 text-white px-2 py-2 mx-2 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+              >
+                <FontAwesomeIcon icon={faUpload} /> Import Data
+              </button>
+
+              {showImportModal && (
+                <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50">
+                  <div className="bg-white p-6 w-11/12 sm:w-3/4 md:w-1/3 rounded-lg shadow-lg flex flex-col">
+                    <h2 className="text-2xl font-semibold mb-4">Import Data</h2>
+                    <div className="flex justify-between mb-4">
+                      <input
+                        type="file"
+                        accept=".xlsx,.xls"
+                        onChange={handleExcelChange}
+                        className="border border-gray-400 p-2 w-full mb-4"
+                      />
+                      <button
+                        onClick={closeImportModal}
+                        className="bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-gray-500"
+                      >
+                        Batal
+                      </button>
+                    </div>
+                    <div className="flex justify-end">
+                      <button
+                        onClick={importExcell}
+                        className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      >
+                        Import
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
-          
           <div className="mt-4 overflow-x-auto rounded-lg border-gray-200">
             <table className="min-w-full bg-white divide-y-2 divide-gray-200 border border-gray-200 table-fixed rounded-xl shadow-lg">
               <thead>
@@ -248,7 +335,10 @@ function Penilaian() {
                   <th className="py-2 px-4 text-center">Aksi</th>
                 </tr>
               </thead>
-              <tbody style={{ backgroundColor: "white" }} className="text-gray-600 text-base font-normal">
+              <tbody
+                style={{ backgroundColor: "white" }}
+                className="text-gray-600 text-base font-normal"
+              >
                 {filteredData.length > 0 ? (
                   filteredData
                     .slice(
@@ -264,7 +354,7 @@ function Penilaian() {
                           {index + 1 + currentPage * dataPerPage}
                         </td>
                         <td className="py-2 px-4 text-center whitespace-nowrap">
-                          {Siswa.find((s) => s.id === item.siswaId)?.nama_siswa}
+                          {siswa.find((s) => s.id === item.siswaId)?.nama_siswa}
                         </td>
                         <td className="py-2 px-4 text-center whitespace-nowrap">
                           {kelas.find((k) => k.id === item.kelasId)?.kelas} -{" "}
@@ -300,7 +390,7 @@ function Penilaian() {
                     ))
                 ) : (
                   <tr>
-                    <td colSpan="6" className="text-center py-4 ">
+                    <td colSpan="6" className="text-center py-4">
                       Tidak ada data ditemukan
                     </td>
                   </tr>
